bbb844c09e04a0a01aaa2f4b883da8b29e6abdf1
Aditya Muraletharan <aditya.muraletharan@outlook.com>
2013-08-25 22:01:45 +0530

    Configured maven to use Github repo

--- src/main/java/com/dilbert/app/App.java
+++ src/main/java/com/dilbert/app/App.java
@@ -4,7 +4,6 @@ import org.jsoup.Connection;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
@@ -48,45 +47,13 @@ public class App
 	 * Save the image
 	 */
 
-	Properties appConfig = new Properties();
-	try {
-		appConfig.load(new FileInputStream("/home/aditya/workspace/dilbert/src/main/resources/application.properties"));
-		log.info("App Config initialized");
-		outputDirectory = appConfig.getProperty(AppConstants.OUTPUT_DIRECTORY);
-		urlHeader = appConfig.getProperty(AppConstants.URL_HEADER);
-		dateFormat = new SimpleDateFormat(appConfig.getProperty(AppConstants.DATE_FORMAT));
-		fileNameFormat = new SimpleDateFormat(appConfig.getProperty(AppConstants.FILE_NAME_FORMAT));
-		startDay = appConfig.getProperty(AppConstants.START_DAY);
-		
-		
-		String conf = appConfig.getProperty(AppConstants.SELECTORS);
-		String[] sels = conf.split(",");
-		log.debug(sels.length);
-		for(String sel : sels) {
-			selectors.add(sel);
-		}
-		
-		log.info("Output directory: " + outputDirectory);
-		log.info("URL Header: " + urlHeader);
-		log.info("Date format: " + appConfig.getProperty(AppConstants.DATE_FORMAT));
-		log.info("Image name format: " + appConfig.getProperty(AppConstants.FILE_NAME_FORMAT));
-		log.info("Start Day: " + startDay);
-		for(String sel : selectors) {
-			log.info("Selection regex while scraping: " + sel);
-		}
-		
-	} catch (FileNotFoundException e1) {
-		log.error("File Not Found Exception: ", e1);
-	} catch (IOException e1) {
-		log.error("IO Exception: " + e1);
-	}
-	
+	readConfig();
 	Long counter = 1L;
 	
 	String currentDay;
 	String endDay = dateFormat.format(new Date());
 	//Testing for first four years
-	endDay = "1993-12-31";
+	endDay = "1989-04-17";
 	
 	try {
 	    Date startDate = dateFormat.parse(startDay);
@@ -95,13 +62,16 @@ public class App
 
 	    Calendar currentCal = Calendar.getInstance();
 	    currentCal.setTime(startDate);
+	    
 	    Calendar endCal = Calendar.getInstance();
 	    endCal.setTime(endDate);
+	    
 	    while(currentCal.compareTo(endCal) <= 0) {
 		//Convert calendar -> date -> string
 		currentDate = currentCal.getTime();
 		currentDay = dateFormat.format(currentDate);
 		log.info("Scraping Image Url for " + currentDay + " from " + urlHeader + currentDay);
+		
 		//Passing URL to ResourceScraper for acquiring the Document with the required image source
 		Long start = System.currentTimeMillis();
 		resourceScraper = new ResourceScraper(urlHeader + currentDay);
@@ -112,6 +82,7 @@ public class App
 		for(Element elmnt : elmnts) {
 		    String imgUrl = elmnt.absUrl("src");
 		    log.info("Image URL : " + imgUrl);
+		    
 		    //Passing image URL to ResourceFetcher for fetching the image
 		    start = System.currentTimeMillis();
 		    resourceFetcher = new ResourceFetcher(imgUrl);
@@ -123,12 +94,8 @@ public class App
 		    try {
 			
 		    	String outDir = buildDirectoryStructure(currentDate);
-		    	log.info("Saving into directory : " + outDir);
-		    	File file = new File(outDir);
-		    	if(!file.exists())
-		    		file.mkdirs();
 		    	String fileName = buildFileName(currentDate);
-		    	file = new File(outDir + fileName);
+		    	File file = new File(outDir + fileName);
 		    	log.info("Saving image to file : " + outDir + fileName);
 		    	FileOutputStream out = new FileOutputStream(file);
 		    	out.write(res.bodyAsBytes());
@@ -150,13 +117,55 @@ public class App
 	}
 	
     }
+    
+    
+    
+    private static void readConfig() {
+    	
+    	Properties appConfig = new Properties();
+    	try {
+    		appConfig.load(App.class.getResourceAsStream("/application.properties"));
+    		log.info("App Config initialized");
+    		outputDirectory = appConfig.getProperty(AppConstants.OUTPUT_DIRECTORY);
+    		urlHeader = appConfig.getProperty(AppConstants.URL_HEADER);
+    		dateFormat = new SimpleDateFormat(appConfig.getProperty(AppConstants.DATE_FORMAT));
+    		fileNameFormat = new SimpleDateFormat(appConfig.getProperty(AppConstants.FILE_NAME_FORMAT));
+    		startDay = appConfig.getProperty(AppConstants.START_DAY);
+    		
+    		
+    		String conf = appConfig.getProperty(AppConstants.SELECTORS);
+    		String[] sels = conf.split(",");
+    		for(String sel : sels) {
+    			selectors.add(sel);
+    		}
+    		
+    		log.info("Output directory: " + outputDirectory);
+    		log.info("URL Header: " + urlHeader);
+    		log.info("Date format: " + appConfig.getProperty(AppConstants.DATE_FORMAT));
+    		log.info("Image name format: " + appConfig.getProperty(AppConstants.FILE_NAME_FORMAT));
+    		log.info("Start Day: " + startDay);
+    		for(String sel : selectors) {
+    			log.info("Selection regex while scraping: " + sel);
+    		}
+    		
+    	} catch (FileNotFoundException e1) {
+    		log.error("File Not Found Exception: ", e1);
+    	} catch (IOException e1) {
+    		log.error("IO Exception: " + e1);
+    	}
+    }
 
 
-    //Extracts the year out of the parameter and returns directory path
+    //Extracts the year out of the parameter and creates the directory structure if it doesn't exist
     private static String buildDirectoryStructure(Date date) {
 	Calendar cal = Calendar.getInstance();
 	cal.setTime(date);
-	return outputDirectory + "/" + String.valueOf(cal.get(Calendar.YEAR)) + "/";
+	String outDir = outputDirectory + "/" + String.valueOf(cal.get(Calendar.YEAR)) + "/";
+	log.info("Saving into directory : " + outDir);
+	File file = new File(outDir);
+	if(!file.exists())
+		file.mkdirs();
+	return outDir;
 	
     }
 
---
+++ src/main/java/com/dilbert/app/AppConstants.java
@@ -0,0 +1,11 @@
+package com.dilbert.app;
+
+public class AppConstants {
+	
+	public static final String OUTPUT_DIRECTORY = "outputdirectory";
+	public static final String  SELECTORS = "selectors";
+	public static final String URL_HEADER = "urlheader";
+	public static final String DATE_FORMAT = "dateformat";
+	public static final String FILE_NAME_FORMAT  = "imagenameformat";
+	public static final String START_DAY = "startday";
+}
